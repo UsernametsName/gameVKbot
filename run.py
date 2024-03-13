e6c1730c146e45aba719145a2d1671b0c255818d
Victor <cozachenko.5@gmail.com>
2024-03-13 12:31:35 +0300

    321 	modified:   run.py

--- run.py
+++ run.py
@@ -1,15 +1,13 @@
 import asyncio
 import os
-from config import VK_API
+#from config import VK_API
 from vkbottle import Bot
-
-vkbot_instance = Bot(token=VK_API)
-async def main():
-    vkbot_instance.run_forever()
+VK_API_TOKEN=""
+vkbot_instance = Bot(token=VK_API_TOKEN)
 
 
 @vkbot_instance.on.message(text=['/start'])
 async def start_command(message):
     await message.answer('Привет, хуесос!')
 
-asyncio.run(main())
+vkbot_instance.run_forever()
