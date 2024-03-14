72436f2e56409985c36e64e5e330f93750dfda95
Victor <cozachenko.5@gmail.com>
2024-03-14 16:58:27 +0300

    1

--- handlers/global_handlers.py
+++ handlers/global_handlers.py
@@ -11,9 +11,8 @@ async def ping_handler(message):
 @labeler.message(command="userinfo")
 async def userinfo_handler(message: Message):
     userid = message.reply_message.from_id if message.reply_message else message.from_id
-    user: UsersUserFull = await message.ctx_api.users.get(userid)
+    user: UsersUserFull = (await message.ctx_api.users.get(userid))[0]
     await message.reply(f"Имя: {user.first_name}\n"
                         f"Фамилия: {user.last_name}\n"
-                        f"Статус: {user.status}\n"
-                        f"Ссылка: {user.last_seen}\n"
+                        f"Ссылка: https://vk.com/id{userid}\n"
                         f"id: {user.id}")
