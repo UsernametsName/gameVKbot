1c56bcff8cb63d41261517bea2cb12614b8bb4f1
Victor <cozachenko.5@gmail.com>
2024-03-20 23:38:58 +0300

    1

--- handlers/privateHandlers.py
+++ handlers/privateHandlers.py
@@ -3,7 +3,7 @@ from vkbottle_types.objects import UsersUserFull
 
 from database.models import Resources
 from database.requests import get_resouses
-
+from keyboards import k_d
 
 
 private_labeler = BotLabeler()
@@ -11,6 +11,5 @@ private_labeler = BotLabeler()
 
 @private_labeler.private_message(command="start")
 async def start_command(message: Message):
-    await message.answer("Приветствую тебя 😊.\nЭто игра ферма 🎮🚜, где именно ты 👈 сможешь развить своё предприятие 🏭📈.🎉")
-    resourses = await get_resouses(message.from_id)
-    await message.answer(f"Монет: {resourses.coins}\nЯиц: {resourses.eggs}\n")
+    await message.answer("Приветствую тебя 😊.\nЭто игра ферма 🎮🚜, где именно ты 👈 сможешь развить своё предприятие 🏭📈.🎉", keyboard=k_d)
+    
