f63d72fe27970fe12e162d132c42942d23111e75
sh1zzzoid <you@example.com>
2024-03-20 21:57:02 +0300

    deleted things in keyboard

--- handlers/keyboard.py
+++ handlers/keyboard.py
@@ -6,12 +6,11 @@ from vkbottle_types.objects import UsersUserFull
 
 kb_labeler = BotLabeler()
 
-@kb_labeler.on.message()
+@kb_labeler.message()
 async def keydef(message: Message):
     keyboard = (
         Keyboard()
         .add(Text("Кнопка 1"))
-        .row()
         .add(Text("Кнопка 2"))
         .row()
         .add(Text("Кнопка 3"))
